e0613eb110ce769080dfce6f1ed1a577036bb06b
Saakshi Gupta <saakshigupta@Saakshis-MacBook-Air.local>
2025-01-11 22:06:44 -0800

    saakshi Commit 2

--- models.py
+++ models.py
@@ -781,8 +781,4 @@ class EmbeddingModelWithAliases:
 def _conversation_name(text):
     # Collapse whitespace, including newlines
     text = re.sub(r"\s+", " ", text)
-    if len(text) <= CONVERSATION_NAME_LENGTH:
-        return text
     return text[: CONVERSATION_NAME_LENGTH - 1] + "…"
-
-#This is commit 3
